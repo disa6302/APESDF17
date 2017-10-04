b4bdbec1dfd73445f711fe5d9e033f8c68e0a9a1
disa6302 <disa6302@colorado.edu>
2017-10-04 03:25:59 -0600

    Multithreaded IO Implementation without graceful exit

--- HW3/multithreading/IO_multithread.c
+++ HW3/multithreading/IO_multithread.c
@@ -1,72 +1,135 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <signal.h>
 
-/*Thread1-Parent, Thread2-Child, Thread3-Child*/
-/*Thread 1-opens a file and writes data into it*/
-/*Thread 2-Reads the file and counts number of words lines, characters in file*/
-/*Thread 3-Prints statistics in human readable format*/
+
 size_t file_size = 0;
-/*struct report {
+struct statistics {
 	size_t words;
 	size_t line;
 	size_t count;
-};*/
+	char **filename
+};
+struct statistics statistic;
+volatile sig_atomic_t processing = 0;
+volatile sig_atomic_t report = 0;
 
+void handle(int signal)
+{
+
+    switch(signal)
+    {
+    	case SIGUSR1:
+    			processing = 1;
+    			break;
+    	case SIGUSR2:
+    			report = 1;
+    			break;
+    }
+}
 void* to_file(void* arg)
 {
+	//struct statistics statistic;
 	char* buff = malloc(1024*sizeof(char));
     memset(buff,0,sizeof(buff));
     FILE* fp = NULL;
-	char** filename = (char**)arg;
-	fp = fopen(filename,"w+");
+	statistic.filename = (char**)arg;
+	fp = fopen(statistic.filename,"w+");
     printf("Enter your text:");
     fgets(buff, 1024*sizeof(char),stdin);
     file_size = fwrite(buff,1,strlen(buff)*sizeof(char),fp);
     printf("Number of characters written into file:%d\n",file_size);
     fclose(fp);
-    pthread_exit(NULL);
 
 }
 
 void* from_file(void* arg)
 {
-    FILE* fp = NULL;
-    int words, line, count;
-    words=line=count=0;
-	char** filename = (char**)arg;
-	fp = fopen(arg,"r+");
-	char* buff = malloc(1024*sizeof(char));
-    memset(buff,0,sizeof(buff));
-    file_size = fread(buff,1,1024*sizeof(char),fp);
-    printf("Number of characters read from file:%d\n",file_size);
-    while(*buff!=NULL)
-    {
-    	count ++;
-    	if(*buff == ' ' || *buff == '\n') words++;
-    	if(*buff == '\n') line++;
+	while(1)
+	{
+	  if(processing)
+      {
+	    FILE* fp = NULL;
+	//    struct statistics statistic;
+	    statistic.words=0;
+	    statistic.count=0;
+	    statistic.line=0;
+		statistic.filename = (char**)arg;
+		fp = fopen(statistic.filename,"r+");
+		char* buff = malloc(1024*sizeof(char));
+	    memset(buff,0,sizeof(buff));
+	    file_size = fread(buff,1,1024*sizeof(char),fp);
+	    printf("Number of characters read from file:%d\n",file_size);
+	    while(*buff!=NULL)
+	    {
+	    	if(*buff != ' ' || *buff !='\n') statistic.count ++;
+	    	if(*buff == ' ' || *buff == '\n') statistic.words++;
+	    	if(*buff == '\n') statistic.line++;
 
-    	buff++;
+	    	buff++;
 
-    }
-    printf("Words:%d\nLines:%d\nCharacters:%d\n",words,line,(count-1));
-    fclose(fp);
-    pthread_exit(NULL);
+	    }
+	    printf("Words:%d\nLines:%d\nCharacters:%d\n",statistic.words,statistic.line,(statistic.count)-1);
+	    fclose(fp);
+	    break;
+	  }	
+	}
+	
+}
+
+void* reporting(void* arg)
+{
+//	struct statistics statistic;
+	while(1)
+	{
+		if(report)
+		{
+			printf("Number of words:%d\n",statistic.words);
+			printf("Number of line:%d\n",statistic.line);
+			printf("Number of characters:%d\n",(statistic.count)-1);
+			break;
+		}
+	}
 }
 
 
 int main(int argc, char** argv)
 {
-  
-  pthread_t write_to_file;
-  pthread_t read_from_file;
-  pthread_attr_t attr;
-  pthread_attr_init(&attr);
-  pthread_create(&write_to_file,&attr,&to_file,(void *)argv[1]);
-  pthread_join(write_to_file,NULL);
-  pthread_create(&read_from_file,&attr,&from_file,(void *)argv[1]);
+	printf("Current PID:%d\n",getpid());
+	pthread_t write_to_file;
+    pthread_t read_from_file;
+    pthread_t report;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    struct sigaction signal_handler;
+    signal_handler.sa_handler = &handle;
+    sigfillset(&signal_handler.sa_mask);
 
+    if(sigaction(SIGUSR1,&signal_handler,NULL) <0)
+    {
+  	  perror("Sigaction error for SIGUSR1\n");
+    }
   
-  pthread_exit(0);
+    if(sigaction(SIGUSR2,&signal_handler,NULL) <0)
+    {
+  	  perror("Sigaction error for SIGUSR2\n");
+    }
+
+ 
+   
+
+
+    pthread_create(&write_to_file,&attr,&to_file,(void *)argv[1]);
+    pthread_join(write_to_file,NULL);
+
+    pthread_create(&read_from_file,&attr,&from_file,(void *)argv[1]);	
+    pthread_kill(read_from_file,SIGUSR1);
+
+    pthread_create(&report,&attr,&reporting,NULL);
+    pthread_join(read_from_file,NULL);
+    pthread_kill(report,SIGUSR2);
+
+    pthread_exit(NULL);
 
 }
